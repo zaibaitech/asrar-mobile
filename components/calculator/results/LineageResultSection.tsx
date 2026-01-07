e49ed937b05b2cebe9e6526118cc4865f2029ee7
zaibaitech <zaibaitech@gmail.com>
2026-01-07 01:44:57 +0000

    chore: Reorder imports in LineageResultSection (formatter)

--- components/calculator/results/LineageResultSection.tsx
+++ components/calculator/results/LineageResultSection.tsx
@@ -4,8 +4,8 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { LineageInsights } from '../../../types/calculator-enhanced';
 import { useLanguage } from '../../../contexts/LanguageContext';
+import { LineageInsights } from '../../../types/calculator-enhanced';
 
 interface LineageResultSectionProps {
   insights: LineageInsights;
